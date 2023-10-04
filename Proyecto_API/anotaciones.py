2f167aefd0772d687842a68077fb8d9683392182
chrisardo <chrisardorolo02@gmail.com>
2023-10-04 18:11:07 -0500

    comando restframework

--- Proyecto_API/anotaciones.py
+++ Proyecto_API/anotaciones.py
@@ -4,4 +4,5 @@ password: 1234
 pip install django-cors-headers
 pip list
 python manage.py createsuperuser #Crear super usuario del panel de administracion de Django
+pip install djangorestframework
 '''
